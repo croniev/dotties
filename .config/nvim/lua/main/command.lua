453cbd8bf19fafe6dd3196ad3eda472f975b8f59
croniev <croniev.git@runbox.com>
2023-10-30 20:47:06 +0100

    nvim fct for running code

--- .config/nvim/lua/main/command.lua
+++ .config/nvim/lua/main/command.lua
@@ -13,3 +13,20 @@ vim.api.nvim_create_user_command('CdParentUnderPwd',
         vim.cmd("cd " .. newd)
     end,
     {})
+
+vim.api.nvim_create_user_command('RunInTerminal',
+    function ()
+        local file = vim.fn.expand('%')
+        local extension = vim.fn.expand('%:e')
+        local commands = {}
+        commands["py"] = "python"
+        commands["sh"] = "bash"
+        commands["js"] = "node"
+        if commands[extension] then
+            vim.cmd("tabnew " .. file)
+            vim.cmd("term " .. commands[extension] .. ' ' .. file)
+        else
+            print("Unknown filetype for running in terminal!")
+        end
+    end,
+    {})
--- .config/nvim/lua/main/remap.lua
+++ .config/nvim/lua/main/remap.lua
@@ -45,17 +45,17 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 -- ???
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 -- deleted stuff does not go into buffer
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- macht iwie nichts
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- Formattieren
 
 -- unsure what cnext, cprev, lnext and lprev do
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
@@ -64,14 +64,13 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- rename what is under the cursor
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true }) -- make the file execcutable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })              -- make the file execcutable
 
 -- Execute file
--- TODO: write a function that 1st detects where to do the terminal and 2nd determindes the filetype
-vim.keymap.set("n", "<leader>r", ":term python %<CR>") -- Python
+vim.keymap.set("n", "<leader>r", ":RunInTerminal<CR>")            -- Python
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/main/packer.lua<CR>"); -- open packer file
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>"); -- find out ;)
+vim.keymap.set("n", "<leader>pac", "<cmd>e ~/.config/nvim/lua/main/packer.lua<CR>"); -- open packer file
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");        -- find out ;)
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
