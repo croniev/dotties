449ad37e532793eba7abbddfd9b1d689f15aaee9
croniev <croniev.git@runbox.com>
2023-11-01 19:21:28 +0100

    dont batterywarn if screen locked

--- Documents/Scripts/batterywarning.sh
+++ Documents/Scripts/batterywarning.sh
@@ -11,17 +11,19 @@ echo $BATTERY_PERCENTAGE
 CABLE_PLUGGED=$(upower -i $LINE_POWER_PATH | grep -A2 'line-power' | grep online | awk '{ print $2 }')
 echo $CABLE_PLUGGED
 
-if [[ $CABLE_PLUGGED == 'no' ]]; then
+if (cinnamon-screensaver-command -q | grep "is active"); then
+    if [[ $CABLE_PLUGGED == 'no' ]]; then
 
-    if [[ $BATTERY_PERCENTAGE -lt 23 ]]; then
-        zenity --warning --title "Battery Low" --text "Low battery ($BATTERY_PERCENTAGE)" --display=:0.0
+        if [[ $BATTERY_PERCENTAGE -lt 23 ]]; then
+            zenity --warning --title "Battery Low" --text "Low battery c$BATTERY_PERCENTAGEc" --display=:0.0
+        fi
+
+
+    else
+
+        if [[ $BATTERY_PERCENTAGE -gt 93 ]]; then
+            zenity --warning --title "Battery Full" --text "Fully charged ($BATTERY_PERCENTAGE)" --display=:0.0
+        fi
+
     fi
-
-
-else
-
-    if [[ $BATTERY_PERCENTAGE -gt 93 ]]; then
-        zenity --warning --title "Battery Full" --text "Fully charged ($BATTERY_PERCENTAGE)" --display=:0.0
-    fi
-
 fi
