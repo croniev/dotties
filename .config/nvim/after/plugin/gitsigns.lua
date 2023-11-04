15c876d13aba0ec75955285224611b259132d0e5
croniev <croniev.git@runbox.com>
2023-11-04 17:59:07 +0100

    gitsigns symbols

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,5 +1,10 @@
 require('gitsigns').setup{
     current_line_blame = true,
+    signs = {
+        add          = { text = '' },
+        change       = { text = '󰜥' },
+        delete = { text = '' },
+    },
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
 
